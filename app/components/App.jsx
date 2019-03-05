b86ed229dc3d9a1b160bf48b5dfc4e5e08c2c165
maria <maluenarod@gmail.com>
2019-03-05 14:11:08 +0100

    GA event added, impact route fixed

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -8,6 +8,8 @@ import getLocation from "lib/location";
 import { setKey, getKey } from "lib/storage";
 import { fetchReachData, fetchImpactData } from "lib/remote";
 
+import { logEvent } from "utils/analytics";
+
 class App extends React.PureComponent {
 
   static contextTypes = {
@@ -51,6 +53,7 @@ class App extends React.PureComponent {
         this.props.navigation.country,
       program: program,
     });
+    logEvent('Program', this.props.navigation.year, program);
   }
 
   handleMapChange(region, country, year) {
--- app/components/Routes.jsx
+++ app/components/Routes.jsx
@@ -7,9 +7,7 @@ import AppWrapper from "components/AppWrapper";
 class Routes extends React.Component {
 
   render() {
-    const isGHPages = process.env.GITHUB_PAGES_FOLDER === 'true' || process.env.GITHUB_PAGES_FOLDER === true;
-    // const prefix = isGHPages ? '/care-international' : '';
-    const prefix = '';
+    const isGHPages = window.location.href.includes('vizzuality.github.io');
     const Router = isGHPages ? HashRouter : BrowserRouter;
 
     // Enabling modal when user go using origin URL
@@ -23,22 +21,20 @@ class Routes extends React.Component {
 
     return (<Router>
       <Switch>
-        <Redirect exact from={`${prefix}/`} to={`${prefix}/2017/reach/countries`} />
-        <Redirect exact from={`${prefix}/reach`} to={`${prefix}/2017/reach/countries`} />
-        <Redirect exact from={`${prefix}/countries`} to={`${prefix}/2017/reach/countries`} />
+        {isGHPages && (<Redirect exact from="/care-international" to="/2018/reach/countries" />)}
+        <Redirect exact from="/" to="/2018/reach/countries" />
+        <Redirect exact from="/reach" to="/2018/reach/countries" />
 
-        <Route exact path={`${prefix}/:year/reach/countries`} component={(props) => <AppWrapper mainView="reach" subView="countries" {...props} />} />
-        <Route exact path={`${prefix}/:year/reach/countries/:country?`} component={(props) => <AppWrapper mainView="reach" subView="countries" {...props} />} />
-        <Route exact path={`${prefix}/:year/reach/regions`} component={(props) => <AppWrapper mainView="reach" subView="regions" {...props} />} />
-        <Route exact path={`${prefix}/:year/reach/regions/:region?`} component={(props) => <AppWrapper mainView="reach" subView="regions" {...props} />} />
-        <Route exact path={`${prefix}/impact/story/:story`} component={(props) => <AppWrapper mainView="impact" {...props} />} />
-        <Route exact path={`${prefix}/impact/:region?/:country?`} component={(props) => <AppWrapper mainView="impact" {...props} />} />
-
+        <Route exact path="/:year/reach/countries" component={(props) => <AppWrapper mainView="reach" subView="countries" {...props} />} />
+        <Route exact path="/:year/reach/countries/:country?" component={(props) => <AppWrapper mainView="reach" subView="countries" {...props} />} />
+        <Route exact path="/:year/reach/regions" component={(props) => <AppWrapper mainView="reach" subView="regions" {...props} />} />
+        <Route exact path="/:year/reach/regions/:region?" component={(props) => <AppWrapper mainView="reach" subView="regions" {...props} />} />
+        <Route exact path="/impact/story/:story" component={(props) => <AppWrapper mainView="impact" {...props} />} />
+        <Route exact path="/impact/:region?/:country?" component={(props) => <AppWrapper mainView="impact" {...props} />} />
         <Route exact path="*" component={(props) => <AppWrapper mainView="notfound" {...props} />} />
       </Switch>
     </Router>);
   }
-
 }
 
 export default Routes;
