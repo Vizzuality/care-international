8ee195e13a3d4bcdf89c6d13ab23e7e0a9082266
maria <maluenarod@gmail.com>
2019-03-11 18:22:43 +0100

    console log removed

--- app/utils/analytics.js
+++ app/utils/analytics.js
@@ -1,8 +1,6 @@
 import ReactGA from 'react-ga';
 
 export const initGA = () => {
-  console.log(process.env.NODE_ENV);
-  console.log(process.env.GOOGLE_ANALYTICS);
   if (process.env.NODE_ENV === 'production') {
     ReactGA.initialize(process.env.GOOGLE_ANALYTICS);
   } else {
