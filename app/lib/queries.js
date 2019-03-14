ece3ef3f85dd9def1a40a19334e2fc4d8d6ecb7b
maria <maluenarod@gmail.com>
2019-03-14 16:38:06 +0100

    queries

--- app/lib/queries.js
+++ app/lib/queries.js
@@ -75,7 +75,6 @@ const getReachMapRegionsSQL = withEscapedArgs((program, year = 2016) => {
 const getReachStatisticsCountriesSQL = withEscapedArgs((country, year = 2016) => {
 
   const suffix = year.toString() === '2016' ? '' : year.toString();
-  // const suffix = year.toString();
 
   let fields = [
     "fnscc_data::BOOL AS has_fnscc_data",
@@ -104,7 +103,17 @@ const getReachStatisticsCountriesSQL = withEscapedArgs((country, year = 2016) =>
     "num_indirect_participants AS overall_indirect_participants",
     "num_projects_and_initiatives AS overall_projects_and_initiatives",
     "COALESCE(percent_women_of_direct_participants, 0) * num_direct_participants AS overall_direct_participants_women",
-    "COALESCE(percent_women_of_indirect_participants, 0) * num_indirect_participants AS overall_indirect_participants_women"
+    "COALESCE(percent_women_of_indirect_participants, 0) * num_indirect_participants AS overall_indirect_participants_women",
+    // "COALESCE(percent_fnscc_women_direct_participants, 0) * num_fnscc_direct_participants AS fnscc_direct_participants_women",
+    // "COALESCE(percent_fnscc_women_indirect_participants, 0) * num_fnscc_indirect_participants AS fnscc_indirect_participants_women",
+    // "COALESCE(percent_hum_women_direct_participants, 0) * num_hum_direct_participants AS hum_direct_participants_women",
+    // "COALESCE(percent_hum_women_indirect_participants, 0) * num_hum_indirect_participants AS hum_indirect_participants_women",
+    // "COALESCE(percent_lffv_women_direct_participants, 0) * num_lffv_direct_participants AS lffv_direct_participants_women",
+    // "COALESCE(percent_lffv_women_indirect_participants, 0) * num_lffv_indirect_participants AS lffv_indirect_participants_women",
+    // "COALESCE(percent_wee_women_direct_participants, 0) * num_wee_direct_participants AS wee_direct_participants_women",
+    // "COALESCE(percent_wee_women_indirect_participants, 0) * num_wee_indirect_participants AS wee_indirect_participants_women",
+    // "COALESCE(percent_srmh_women_direct_participants, 0) * num_srmh_direct_participants AS srmh_direct_participants_women",
+    // "COALESCE(percent_srmh_women_indirect_participants, 0) * num_srmh_indirect_participants AS srmh_indirect_participants_women",
   ];
 
   return `SELECT ${fields.join(", ")} FROM reach_data${suffix} WHERE country = '${country || "Total"}'`;
@@ -140,7 +149,17 @@ const getReachStatisticsRegionsSQL = withEscapedArgs((region, year = 2016) => {
     "SUM(num_indirect_participants) AS overall_indirect_participants",
     "SUM(num_projects_and_initiatives) AS overall_projects_and_initiatives",
     "SUM(COALESCE(percent_women_of_direct_participants, 0) * num_direct_participants) AS overall_direct_participants_women",
-    "SUM(COALESCE(percent_women_of_indirect_participants, 0) * num_indirect_participants) AS overall_indirect_participants_women"
+    "SUM(COALESCE(percent_women_of_indirect_participants, 0) * num_indirect_participants) AS overall_indirect_participants_women",
+      // "SUM(COALESCE(percent_fnscc_women_direct_participants, 0) * num_fnscc_direct_participants) AS fnscc_direct_participants_women",
+      // "SUM(COALESCE(percent_fnscc_women_indirect_participants, 0) * num_fnscc_indirect_participants) AS fnscc_indirect_participants_women",
+      // "SUM(COALESCE(percent_hum_women_direct_participants, 0) * num_hum_direct_participants) AS hum_direct_participants_women",
+      // "SUM(COALESCE(percent_hum_women_indirect_participants, 0) * num_hum_indirect_participants) AS hum_indirect_participants_women",
+      // "SUM(COALESCE(percent_lffv_women_direct_participants, 0) * num_lffv_direct_participants) AS lffv_direct_participants_women",
+      // "SUM(COALESCE(percent_lffv_women_indirect_participants, 0) * num_lffv_indirect_participants) AS lffv_indirect_participants_women",
+      // "SUM(COALESCE(percent_wee_women_direct_participants, 0) * num_wee_direct_participants) AS wee_direct_participants_women",
+      // "SUM(COALESCE(percent_wee_women_indirect_participants, 0) * num_wee_indirect_participants) AS wee_indirect_participants_women",
+      // "SUM(COALESCE(percent_srmh_women_direct_participants, 0) * num_srmh_direct_participants) AS srmh_direct_participants_women",
+      // "SUM(COALESCE(percent_srmh_women_indirect_participants, 0) * num_srmh_indirect_participants) AS srmh_indirect_participants_women",
   ];
 
   return `SELECT ${fields.join(", ")} FROM reach_data${suffix} WHERE region = '${region}'`;
@@ -203,6 +222,7 @@ const getImpactRegionDataSQL = withEscapedArgs((region) => {
 
   let subquery = `SELECT ${subfields.join(", ")} FROM impact_data2018`;
 
+
   if (!region) {
     subquery += " GROUP BY region";
   } else {
@@ -237,19 +257,18 @@ const getImpactStoriesSQL = withEscapedArgs(() => {
     `SELECT ${fields.join(", ")}`,
     "FROM story_new s INNER JOIN (",
     `  SELECT story_number, ${bounds}`,
-    "  FROM story_new s INNER JOIN impact_data2018 i ON s.iso = i.iso",
+    "  FROM story_new s INNER JOIN impact_data2017 i ON s.iso = i.iso",
     "  GROUP BY story_number",
     ") g ON s.story_number = g.story_number",
     "GROUP BY s.story_number, s.the_geom",
   ].join(" ");
 });
 
-const getImpactStoriesByCountrySQL = withEscapedArgs((country) => {
+const getImpactStoriesByCountrySQL = withEscapedArgs(() => {
   const fields = [
     "s.story_number AS story_number",
     "ST_AsGeoJSON(s.the_geom) as country_centroid",
     "s.country AS country",
-    "s.region AS region",
     "AVG(ST_X(the_geom)) AS lon",
     "AVG(ST_Y(the_geom)) AS lat",
     "MIN(story) AS story",
@@ -259,8 +278,7 @@ const getImpactStoriesByCountrySQL = withEscapedArgs((country) => {
   return [
     `SELECT ${fields.join(", ")}`,
     "FROM story_new s",
-    `${country ? ` WHERE s.country ='${country}'` : ''}`,
-    "GROUP BY s.story_number, s.country, s.region, s.the_geom",
+    "GROUP BY s.story_number, s.country, s.the_geom",
   ].join(" ");
 });
 
