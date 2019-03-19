24fb95828a0b23dfc7cf4d8d6118e634686ced1a
maria <maluenarod@gmail.com>
2019-03-19 16:04:50 +0100

    ranges

--- app/components/elements/ValueBar/index.jsx
+++ app/components/elements/ValueBar/index.jsx
@@ -4,9 +4,6 @@ import classNames from "classnames";
 
 import "./style.scss";
 
-const maxWidth = 180;
-const minWidth = 10;
-
 class ValueBar extends React.Component {
 
   static propTypes = {
@@ -16,12 +13,48 @@ class ValueBar extends React.Component {
   };
 
   render() {
+
+    let widthValue;
+
+    if (0 < this.props.value && this.props.value <= 1.000) {
+      widthValue = 16;
+    }
+    else if (1.000 < this.props.value && this.props.value <= 5.000) {
+      widthValue = 32;
+    }
+    else if (5.000 < this.props.value && this.props.value <= 10.000) {
+      widthValue = 48;
+    }
+    else if (10.000 < this.props.value && this.props.value <= 20.000) {
+      widthValue = 64;
+    }
+    else if (20.000 < this.props.value && this.props.value <= 50.000) {
+      widthValue = 80;
+    }
+    else if (50000 < this.props.value && this.props.value <= 100000) {
+      widthValue = 96;
+    }
+    else if (100000 < this.props.value && this.props.value <= 500000) {
+      widthValue = 112;
+    }
+    else if (500000 < this.props.value && this.props.value <= 1000000) {
+      widthValue = 128;
+    }
+    else if (1000000 < this.props.value && this.props.value <= 10000000) {
+      widthValue = 144;
+    }
+    else if (10000000 < this.props.value && this.props.value <= 50000000) {
+      widthValue = 160;
+    }
+    else {
+      widthValue = 176;
+    }
+
     let styles = {
       current: {
-        width: (maxWidth * this.props.value / (this.props.maxValue || 1) + minWidth)  + "px",
+        width: widthValue + "px",
       },
     };
-
     let barClasses = classNames(["valuebar", this.props.colorClass]);
 
     return (<div className="valuebar__container">
